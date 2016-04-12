3515361aa8e4ece22c34a7f0f84f2643c2462217
Caden Howell <cadenh@benetech.org>
2016-04-12 14:16:50 -0700

    Working webapp only, doesn't do anything, added port 8765

---
+++ authenticonClientApp/src/main/java/org/benetech/authenticon/AuthenticonClientController.java
@@ -0,0 +1,22 @@
+package org.benetech.authenticon;
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class AuthenticonClientController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AuthenticonClientController.class, args);
+    }
+
+}
